1263f4b64c64280a5e7747d15604236dc044be05
SenkeSem <sen9morf@gmail.com>
2023-04-24 23:36:43 +0300

    v1.1

--- arr.js
+++ arr.js
@@ -1,9 +1,6 @@
 let x = prompt("Введите число x", 14);
 let n = prompt("Введите степень, в которую хотите возвести число x", 2);
 
-console.log(x);
-console.log(n);
-
 console.log("Пользователь сказал, что x=" + x);
 console.log("Пользователь хочет возвести x в " + n + " степень");
 
@@ -11,4 +8,4 @@ console.log("Пользователь хочет возвести x в " + n + "
     return Math.pow(x, n);
 }
 
-console.log("Результат вычисления функции: " + pow(x, n));
+alert(pow(x, n));
